5a51bbf1d1a064d39f415eba14cf3fcf548768ef
Mayank Mahajan <mayankm2089@gmail.com>
2024-08-28 20:46:40 +0530

    feat(wakatime): change text format to "⏳W h:mm"

--- wakahours@imeleo.com/extension.js
+++ wakahours@imeleo.com/extension.js
@@ -1,53 +1,57 @@
-const {St} = imports.gi;
+const { St } = imports.gi;
 const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
 
-const REFRESH_INTERVAL_SECONDS = 60 * 5
+const REFRESH_INTERVAL_SECONDS = 60 * 5;
 
 let panelButton = new St.Bin({
-    style: 'padding-top: 7px',
+  style: "padding-top: 7px",
 });
 
 let panelButtonText = new St.Label({
-    text: getPanelButtonText(getWakatimeValue()),
+  text: getPanelButtonText(getWakatimeValue()),
 });
 
 function disable() {
-    Main.panel._rightBox.remove_child(panelButton);
+  Main.panel._rightBox.remove_child(panelButton);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
-    updatePanelButtonText();
-    GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, REFRESH_INTERVAL_SECONDS, () => {
-        updatePanelButtonText();
-        return true; // Continue the timeout
-    });
+  Main.panel._rightBox.insert_child_at_index(panelButton, 0);
+  updatePanelButtonText();
+  GLib.timeout_add_seconds(
+    GLib.PRIORITY_DEFAULT,
+    REFRESH_INTERVAL_SECONDS,
+    () => {
+      updatePanelButtonText();
+      return true; // Continue the timeout
+    },
+  );
 }
 
 function getWakatimeValue() {
-    var [ok, commandOutputBytes] = GLib.spawn_command_line_sync('wakatime --today');
-    if (!ok) {
-        return "Error";
-    }
-    return commandOutputBytes.toString();
+  var [ok, commandOutputBytes] =
+    GLib.spawn_command_line_sync("wakatime --today");
+  if (!ok) {
+    return "Error";
+  }
+  let commandOutput = commandOutputBytes.toString();
+  // sample output: ['4 hrs 2 mins','51 mins']
+  let hours = commandOutput.match(/(\d+) hrs/);
+  hours = hours ? hours[1] : "0";
+  let minutes = commandOutput.match(/(\d+) mins/);
+  minutes = minutes ? minutes[1] : "00";
+  return `${hours}:${minutes}`;
 }
 
 function getPanelButtonText(wakaTimeValue) {
-    return `⏳${wakaTimeValue}`
+  return `⏳W ${wakaTimeValue}`;
 }
 
 function init() {
-
-    panelButton.set_child(panelButtonText);
+  panelButton.set_child(panelButtonText);
 }
 
 function updatePanelButtonText() {
-    panelButtonText.set_text(getPanelButtonText(getWakatimeValue()));
+  panelButtonText.set_text(getPanelButtonText(getWakatimeValue()));
 }
-
-
-
-
-
-
